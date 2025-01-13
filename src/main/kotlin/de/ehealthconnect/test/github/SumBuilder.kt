72bd8a244050c2879564b26ac8582f94571285fd
corinne.fehlauer <corinne.fehlauer@ehealth-connect.de>
2025-01-13 15:50:45 +0100

    test Github actions

--- src/main/kotlin/de/ehealthconnect/test/github/SumBuilder.kt
+++ src/main/kotlin/de/ehealthconnect/test/github/SumBuilder.kt
@@ -1,5 +1,5 @@
 package de.ehealthconnect.test.github
 
 class SumBuilder {
-    fun sum(first: Int, second: Int): Int = first + second
+    fun sum(first: Int, second: Int): Int = first + seconds
 }
