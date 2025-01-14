4343c5dc4c004a5115b12999b6ab0ec8ce946f4b
corinne.fehlauer <corinne.fehlauer@ehealth-connect.de>
2025-01-14 08:05:19 +0100

    test coverage

--- src/main/kotlin/de/ehealthconnect/test/github/SumBuilder.kt
+++ src/main/kotlin/de/ehealthconnect/test/github/SumBuilder.kt
@@ -2,4 +2,6 @@ package de.ehealthconnect.test.github
 
 class SumBuilder {
     fun sum(first: Int, second: Int): Int = first + second
+
+    fun uncovered(first: Int, second: Int): Int = first + second + 2
 }
